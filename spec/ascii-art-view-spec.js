f8076f6602ed08466a3727ead37c8933d4f59660
Michael Tyndall <rmt4897@live.unc.edu>
2020-02-19 18:19:24 -0500

    initial folder with files

---
+++ spec/ascii-art-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AsciiArtView from '../lib/ascii-art-view';
+
+describe('AsciiArtView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
