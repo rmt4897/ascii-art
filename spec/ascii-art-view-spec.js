e8371d58e58ee54e12ff8faeb8dc818af54aa74d
Michael Tyndall <rmt4897@live.unc.edu>
2020-02-19 18:23:00 -0500

    removed unnecessary files

--- spec/ascii-art-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AsciiArtView from '../lib/ascii-art-view';
-
-describe('AsciiArtView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
